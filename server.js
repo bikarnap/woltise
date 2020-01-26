1d254167640027b9713e2bb6aab40432f9ec9908
Bikarna Pokharel <bikarna.pokharel@tuni.fi>
2020-01-26 23:08:30 +0200

    Update server.js Added a function to return distance between two locations based on latitudes and longitudes

--- server.js
+++ server.js
@@ -13,6 +13,22 @@ const port = process.env.PORT || 8080;
 // Test api 
 // --> /restaurants/search?q=sushi&lat=60.17045&lon=24.93147
 
+// function to find the distance between two locations
+const getDistance = (lat1, lat2, lon1, lon2) => {
+    const radius = 6371e3; // radius of the earth in meters
+    let diffLat = ((lat2 - lat1) * Math.PI)/180; // difference in latitudes converted to radians
+    let diffLon = (lon2 - lon1) * Math.PI/180; // difference in longitudes converted to radians
+    let phi1 = lat1 * Math.PI/180;
+    let phi2 = lat2 * Math.PI/180;
+    let a = Math.sin(diffLat/2) * Math.sin(diffLat/2) +
+          Math.cos(phi1) * Math.cos(phi2) *
+          Math.sin(diffLon/2) * Math.sin(diffLon/2);
+    let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+  
+    let distance = radius * c;
+    return distance;
+  }
+
 // GET all the restaurants
 app.get('/restaurants', (req, res) => {
     fs.readFile(fileUrl, 'utf-8', (error, data) => {
@@ -33,6 +49,7 @@ app.get('/restaurants/search', (req, res) => {
             const tags = restaurant.tags.includes(queryParams.q);
             const lat = restaurant.location.includes(parseFloat(queryParams.lat));
             const lon = restaurant.location.includes(parseFloat(queryParams.lon));
+            const 
             if(tags && lat && lon) return restaurant;
         })
         if(queryResults == "") res.status(404).send("No restaurants found with the given queries");
