277513adb8938116f6eb9c66d8c0cbbb24c9f47c
Bikarna Pokharel <bikarna.pokharel@tuni.fi>
2020-01-28 00:47:26 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,17 +5,9 @@ const fileUrl = 'restaurants.json';
 const app = express(); 
 const port = process.env.PORT || 8080;
 
-
-// References
-// 404 not found
-
-
-// Test api 
-// --> /restaurants/search?q=sushi&lat=60.17045&lon=24.93147
-
 // function to find the distance between two locations
 const getDistance = (lat1, lat2, lon1, lon2) => {
-    const radius = 6371e3; // radius of the earth in meters
+    const radius = 6371; // radius of the earth in kilometers
     let diffLat = ((lat2 - lat1) * Math.PI)/180; // difference in latitudes converted to radians
     let diffLon = (lon2 - lon1) * Math.PI/180; // difference in longitudes converted to radians
     let phi1 = lat1 * Math.PI/180;
@@ -26,8 +18,35 @@ const getDistance = (lat1, lat2, lon1, lon2) => {
     let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
   
     let distance = radius * c;
-    return distance;
+    return (distance); //in km
   }
+
+
+const processQueryString = (tags, name, description, query) => {
+    if(query.length >=1) {
+        return tags.join('').toUpperCase().includes(query.toUpperCase()) ||
+                name.split(' ').join('').toUpperCase().includes(query.toUpperCase())||
+                description.split(' ').join('').toUpperCase().includes(query.toUpperCase());
+    }
+  }
+
+
+  const processDistance = (lat1, lat2, lon1, lon2) => {
+    console.log("Distance: ",getDistance(lat1, lat2, lon1, lon2) )
+    if(getDistance(lat1, lat2, lon1, lon2) < 3) 
+        return true; 
+    else 
+        return false;
+  }
+    
+
+const processQueryResults = (restaurants, query, lat, lon) => {
+     const results = restaurants.filter(restaurant => 
+        processQueryString(restaurant.tags, restaurant.name, restaurant.description, query) &&
+            processDistance(lat, restaurant.location[1], lon, restaurant.location[0])
+     )
+     return results;
+}
 
 // GET all the restaurants
 app.get('/restaurants', (req, res) => {
@@ -43,26 +62,17 @@ app.get('/restaurants', (req, res) => {
 app.get('/restaurants/search', (req, res) => {
     fs.readFile(fileUrl, 'utf-8', (error, data) => {
         if(error) throw error.message;
-        const restaurants = JSON.parse(data);
-        const queryParams = req.query;
-        const queryResults = restaurants["restaurants"].filter(restaurant => {
-            const tags = restaurant.tags.includes(queryParams.q);
-            const lat = restaurant.location.includes(parseFloat(queryParams.lat));
-            const lon = restaurant.location.includes(parseFloat(queryParams.lon));
-            const 
-            if(tags && lat && lon) return restaurant;
-        })
-        if(queryResults == "") res.status(404).send("No restaurants found with the given queries");
-        else res.send(queryResults);
+        const restaurants = JSON.parse(data)["restaurants"];
+        const searchParams = req.query;
+        const queryResults = processQueryResults(restaurants,searchParams.q, searchParams.lat, searchParams.lon);
+        if(queryResults.length < 1) res.status(404).send(
+            "No restaurants found with the given queries");
+        else res.send({"restaurants":queryResults});      
     })
 })
-
-
-
 
 // Listen to server 
 app.listen(port, () => console.log(`Server listening on port ${port}`)); 
 
-
-
-
+// Test api 
+// --> /restaurants/search?q=sushi&lat=60.17045&lon=24.93147
