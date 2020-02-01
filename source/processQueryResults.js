64f412fa80c97faf6e1e065ea5742ed887cdedfb
Bikarna Pokharel <bikarna.pokharel@tuni.fi>
2020-02-01 13:59:47 +0200

    Fixed some indentation

--- source/processQueryResults.js
+++ source/processQueryResults.js
@@ -8,8 +8,10 @@ const processDistance = require('./processDistance');
 const processQueryResults = (restaurants, query, lat, lon) => {
     if(query.length >= 1) {
         const results = restaurants.filter(restaurant => 
-            processQueryString(restaurant.tags, restaurant.name, restaurant.description, query) &&
-                processDistance(lat, restaurant.location[1], lon, restaurant.location[0])
+            processQueryString(restaurant.tags, restaurant.name, 
+                                restaurant.description, query) &&
+                processDistance(lat, restaurant.location[1], 
+                                lon, restaurant.location[0])
          )
          return results;
     }
